eb00aefa3fb5fc112108d2476179005c16efdd49
mamatagurung <mamatagurung190@gmail.com>
2022-05-06 21:42:14 +0545

    insert read and delete query code

---
+++ backend.php
@@ -0,0 +1,76 @@
+<?php
+
+$servername = "localhost";
+$username = "mamata";
+$password = "sv50/tXJxd.XOQU4";
+$dbname = "crudoperation";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+
+extract($_POST);
+//display data
+if(isset($_POST['readrecord'])){
+    $data = '<table class="table table-bordered table-striped">
+    <tr>
+    <th>No.</th>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Email Address</th>
+    <th>Mobile Number</th>
+    <th>Edit Action</th>
+    <th>Delete Action</th>
+    </tr>';
+    $displayquery = "SELECT * FROM `crudtable`";
+    $result = mysqli_query($conn,$displayquery);
+
+    if(mysqli_num_rows($result)>0){
+      $number = 1;
+       while($row = mysqli_fetch_array($result)){
+
+        $data .= '<tr>
+        <td>'.$number.'</td>
+        <td>'.$row['firstname'].'</td>
+        <td>'.$row['lastname'].'</td>
+        <td>'.$row['email'].'</td>
+        <td>'.$row['phone'].'</td>
+        <td>
+        <button onclick="getUserDetails('.$row['id'].')" class="btn btn-warning">Edit</button>
+        </td>
+        <td>
+        <button onclick="deleteUser('.$row['id'].')" class="btn btn-warning">Delete</button>
+        </td>
+        </tr>';
+        $number++;
+       }
+    }
+    $data .='</table>';
+    echo $data;
+
+
+}
+
+//insert data
+if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['mobile'])){
+    $query = "INSERT INTO `crudtable` (`firstname`, `lastname`, `email`, `mobile`) VALUES ('$firstname','$lastname','$email','$mobile');";
+    mysqli_query($conn,$query);
+}
+
+// $sql = "INSERT INTO `crudtable` (`id`, `firstname`, `lastname`, `email`, `mobile`) VALUES (NULL, 'Milan', 'Gurung', 'milangurung@gmail.com', '9803582104');";
+// mysqli_query($conn,$sql);
+
+
+//delete user record
+
+if(isset($_POST['deleteid'])){
+  echo "it works";
+  $userid = $_POST['deleteid'];
+  $deletequery = "DELETE FROM `crudtable` WHERE `crudtable`.`id` = '$userid'";
+  mysqli_query($conn,$deletequery);
+}
+?>
---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CRUD Operation using AJAX in PHP</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  
+</head>
+<body>
+    
+   <div class="container">
+       <h1 class="text-primary text-uppercase text-center">AJAX CRUD OPERATION</h1>
+       <div class="d-flex justify-content-end">
+            <!-- Button to Open the Modal -->
+          <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#myModal">Open Model</button>
+
+       </div>
+       <h2 class="text-danger">All Records</h2>
+       <div id="records_contant"></div>
+       <!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">AJAX CRUD OPERATION</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+    
+      <!-- Modal body -->
+      <div class="modal-body">
+       <div class="form-group">
+          <label for="Firstname">Firstname:</label>
+          <input type="text" name="" id="firstname" class="form-control" placeholder="First Name">
+       </div>
+       <div class="form-group">
+           <label for="Lastname">Lastname:</label>
+           <input type="text" name="" id="lastname" class="form-control" placeholder="Last Name">
+           </div>
+           <div class="form-group">
+           <label for="email">Email ID:</label>
+           <input type="text" name="" id="email" class="form-control" placeholder="Email">
+           </div>
+           <div class="form-group">
+           <label for="Phone Number">Mobile:</label>
+           <input type="text" name="" id="mobile" class="form-control" placeholder="Mobile Number">   
+       </div>
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+      <button type="button" class="btn btn-danger" data-dismiss="modal" onclick="addRecord()">Save</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div> 
+   </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script type="text/javascript">
+
+      $(document).ready(function(){
+          readRecords();
+      });
+       function readRecords(){
+           var readrecord = "readrecord";
+
+           $.ajax({
+               url:"backend.php",
+               type:"post",
+               data:{ readrecord:readrecord},
+               success:function(data,status){
+                   $('#records_contant').html(data);
+               }
+           });
+       };
+      function addRecord(){
+          var firstname = $('#firstname').val();
+          var lastname = $('#lastname').val();
+          var email = $('#email').val();
+          var mobile = $('#mobile').val();
+
+          $.ajax({
+              url: "backend.php",
+              type: 'post',
+              data: {firstname:firstname,
+                  lastname:lastname,
+                  email:email,
+                  mobile:mobile
+              },
+              success:function(data,status){
+                  readRecords();
+              },
+          });
+      };
+
+      function deleteUser(deleteid){
+          var conf = confirm("Are you sure about deleting the data!!!");
+          if(conf===true){
+              $.ajax({
+                  url: "backend.php",
+                  type: "post",
+                  data:{ deleteid:deleteid},
+                  success:function(data,status){
+                      readRecords();
+                  },
+              });
+              
+          }
+      }
+  </script>
+</body>
+</html>
